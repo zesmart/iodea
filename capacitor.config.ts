21305ed3f40bf35f777bd31cd2bb91798f58b09a
zesmart <15648640997@163.com>
2026-01-22 16:13:31 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iodea.app',
+  appName: 'iodea',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // 监听所有地址，包括局域网
+    port: 5173, // 端口号
+    open: false // 不自动打开浏览器
+  }
+})
