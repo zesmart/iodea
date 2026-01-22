ec765fc1b0dda8359b38062e316d94bceea31bf8
zesmart <15648640997@163.com>
2026-01-22 20:33:09 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,33 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0', // 监听所有地址，包括局域网
-    port: 5173, // 端口号
-    open: false // 不自动打开浏览器
+    port: 5173, // 端口号,
+    open: false,
+    proxy: {
+      '^/api/zhihu/(.*)': {
+        target: 'https://zhida.zhihu.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/zhihu/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyRes', (proxyRes) => {
+            delete proxyRes.headers['x-frame-options']
+            delete proxyRes.headers['content-security-policy']
+            delete proxyRes.headers['x-content-type-options']
+          })
+        }
+      },
+      '^/api/metaso/(.*)': {
+        target: 'https://metaso.cn',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/metaso/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyRes', (proxyRes) => {
+            delete proxyRes.headers['x-frame-options']
+            delete proxyRes.headers['content-security-policy']
+            delete proxyRes.headers['x-content-type-options']
+          })
+        }
+      }
+    }
   }
 })
